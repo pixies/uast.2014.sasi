4ef981e054511df9b8b5777d083ae88cb5b65271
p1xies <cleyton@tagi.com.br>
2014-10-31 12:16:38 -0300

    Leia primeiro

---
+++ LEIAME.rb
@@ -0,0 +1,7 @@
+Material Estudos (UFRPE/UAST - SI - SASI)
+=========================================
+
+SASI - Segurança e Auditoria de Sistemas de Informação
+----
+
+Aqui estarão disponívels para download materiais utilizados na disciplina.
